f7ee525c7ea70c09c1be5a3d1b757bfa7c59cf06
lenaaviles <lenaricabal@gmail.com>
2017-12-08 13:43:21 +0200

    FizzBuzz fix

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -6,17 +6,17 @@
 
 var max = 100;
 var output;
-document.write("for loop<br>");
+document.write("<br>For loop<br>");
 for (var i = 1; i <=100; i++) {
     output = "";
-    output = i % 3 == 0 ? "Fizz" : ""; 
-    output += i % 5 == 0 ? "Buzz" : "";
+    i % 3 == 0 && (output = "Fizz"); 
+    i % 5 == 0 && (output += "Buzz");
     output = output || i;
     
     document.write(i + " = " + output+", ");
 }
 
-document.write("<br>while loop<br>");
+document.write("<br><br>While loop<br>");
 
 i = 1;
 while (i <=100) {
@@ -29,7 +29,7 @@ while (i <=100) {
     i++;
 }
 
-document.write("<br>do-while loop<br>");
+document.write("<br><br>Do-while loop<br>");
 
 i = 1;
 do {
